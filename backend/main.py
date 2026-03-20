20629695b644b7fd2a936056f8583dc8a297b314
Randel Lontok <randeljonhlontok@gmail.com>
2026-03-20 23:41:27 +0800

    main.py origins updated

--- backend/main.py
+++ backend/main.py
@@ -111,7 +111,7 @@ app = FastAPI(title="EE Licensure Predictor API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "ee-predictor.netlify.app",
+        "https://ee-predictor.netlify.app",
         "http://localhost:3000",  # keep for local dev
     ],
     allow_credentials=True,
